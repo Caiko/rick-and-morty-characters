7174ef9e3860df72731583521b0efd31512daed6
Marin <marinenikolov@gmail.com>
2025-06-05 19:01:50 +0200

    Changed Titme and Fav Icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import FullTable from "./Components/FullTable";
 function App() {
   return (
     <>
-      <div className="w-screen h-screen bg-[url('./assets/ricks-toilet.png')] bg-cover bg-center flex items-center justify-center">
+      <div className="w-screen h-screen bg-[url('/ricks-toilet.png')] bg-cover bg-center flex items-center justify-center">
         <div className="h-4/5 w-4/5">
           <FullTable />
         </div>
